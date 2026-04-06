4d576f20bdad7d07e31ed64e6ab9311b8b2127e9
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-04-06 22:09:06 +0530

    React Routing

--- Reactjs/Ecommerce Web/src/App.jsx
+++ Reactjs/Ecommerce Web/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
-import {HomePage} from './pages/HomePage'
+import {Routes,Route} from 'react-router';
+import './App.css';
+import {HomePage} from './pages/HomePage';
+import { Checkout } from './pages/Checkout';
+import{OrderPage} from './pages/OrderPage';
+import{Tracking} from './pages/Tracking';
 
 
 function App() {
   
 
   return (
-    <HomePage/>
+    <>
+    <Routes>
+      <Route index element={ <HomePage/>}/>
+      <Route path="checkout" element={ <Checkout/>}/>
+      <Route path="order" element={<OrderPage/>}/>
+      <Route path='tracking' element={<Tracking/>}> </Route>
+    </Routes>
+    </>
+   
   );
 }
 
