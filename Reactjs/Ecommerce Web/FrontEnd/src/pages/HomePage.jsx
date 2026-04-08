576dc382585c029beba7f47d5a91d1c3c619a98c
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-04-08 09:51:34 +0530

    fetching data from backend

--- Reactjs/Ecommerce Web/FrontEnd/src/pages/HomePage.jsx
+++ Reactjs/Ecommerce Web/FrontEnd/src/pages/HomePage.jsx
@@ -3,6 +3,15 @@ import './HomePage.css';
 import {products} from '../../data/products.js';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products').then(
+    (response)=>{
+        response.json().then(
+          (data)=>{
+              console.log(data);
+          }
+        )
+    }
+  )
   return (
     <>
 
